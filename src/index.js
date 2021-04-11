91ce8c9be91ee7bb65e23d7d4cff5bfd563ff2e4
Furyx-s <70293686+Furyx-s@users.noreply.github.com>
2021-04-12 00:46:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ app.listen(3242);
 
 const { ShardingManager, MessageEmbed } = require('discord.js');
 const shards = new ShardingManager('./shard-bot.js', {
-  token: require("./utils/data.json").token.discord,
+  token: require("ODMwODk2MjYwNjgzNzkyNDk0.YHNWcQ.drxPm9C8-_a7A0qQTfXTJ_OrkqY").token.discord,
   totalShards: "auto",
   autoSpawn: true
 });
 
 shards.on('shardCreate', shard => console.log(`[SHARD-${shard.id}] Executed... ${Math.floor(shard.id + 1)}/${shards.totalShards}`));
-
 shards.spawn();
